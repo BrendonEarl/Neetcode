6e235fbe1856498ceaa670d51cc3abf911a7d65d
brendon earl <brendon@brendons-MacBook-Pro.local>
2026-08-10 12:42:47 -0600

    log

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Neetcode
+
+## Practice Log
+
+| Date | Problem                 | Pattern | Key Insight                                 | Conf |
+|------|-------------------------|---------|---------------------------------------------|:----:|
+| 2026-08-10 | List Contains Duplicate | Hashing | hash allows constant complexity O(1) lookup | 3 |
--- Solution.py
+++ Solution.py
@@ -2,20 +2,16 @@ from typing import List
 
 
 class Solution:
-    #num occurances in list -> hash
-    def hasDuplicate(self, nums: List[int]) -> bool:
+    #count occurances in list -> hash
+    #
+    def hasDuplicate(self, nums: List[int]) ->bool:
         d = {}
         for n in nums:
             d[n] = 0
         for n in nums:
             d[n] += 1
-            if d[n] > 1:
-                return True
-
+            if d[n] > 1: return True
         return False
-
-
-
 
 nums = [1, 2, 3, 3]
 print(Solution().hasDuplicate(nums))
