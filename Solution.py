9697722a996a17685e87fcc4db3f7904cb9cf7ae
brendon earl <brendon@brendons-MacBook-Pro.local>
2026-08-19 13:40:59 -0600

    is valid anagram: learned dictionary().get(ket, default) as standard setup

--- Solution.py
+++ Solution.py
@@ -2,8 +2,7 @@ from typing import List
 
 
 class Solution:
-    #count occurances in list -> hash
-    #
+    # count occurances in list -> hash
     def hasDuplicate(self, nums: List[int]) ->bool:
         d = {}
         for n in nums:
@@ -13,5 +12,19 @@ class Solution:
             if d[n] > 1: return True
         return False
 
-nums = [1, 2, 3, 3]
-print(Solution().hasDuplicate(nums))
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        d = {}
+
+        for c in d:
+            d[c] = d.get(c,0) + 1
+
+        for n in t:
+            if n not in d.keys(): return False
+            d[n] -= 1
+        if all((x==0) for x in d.values()): return True
+        return False
+        
+
+Solution().isAnagram(s = "racecar", t = "carrace")
